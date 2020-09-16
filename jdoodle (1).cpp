70f90c9c27ea69b4a046e4852c0f5b88759f87c9
shivanikamble16 <70128827+shivanikamble16@users.noreply.github.com>
2020-09-16 20:48:44 +0530

    Add files via upload

---
+++ jdoodle (1).cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+        int num;
+        cout<<"Enter the number:\n";
+        cin>>num;
+      cout<<"The number entered by user is "<<num;
+        return 0;
+ }
